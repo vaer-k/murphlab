9cb35ef8b264362e76d3d2b1aa1f69533829da5f
vincentruppjr <vincentruppjr@gmail.com>
2013-05-05 13:26:03 -0700

    cleaning house

--- reconitall.sh
+++ reconitall.sh
@@ -12,7 +12,7 @@ echo -n "What email address do you want your processing updates sent to?:"
 
 read EMAIL
 
-echo -n "Okay, got the FreeSurfer segmentation for ${ID} going. I believe it should be finished in approximately 23 years, err, I mean hours, yeah hours. Don't worry, I'll email you when it's done." | mail -s "The FreeSurfer segmentation process for ${ID} has begun" ${EMAIL}
+echo -n "Okay, got the FreeSurfer segmentation for ${ID} going. It should be complete in approx 24 hours. You'll receive an email when it's done." | mail -s "The FreeSurfer segmentation process for ${ID} has begun" ${EMAIL}
 
 NIFTI=`echo "$BRIKFILE" | sed 's/\.BRIK.*//'`  #strip BRIK suffix
 
@@ -27,6 +27,6 @@ if [ $a == a ]
 then 
 recon-all -i ${RETURNDIR}${ID}.nii.gz -subject ${ID} -all
 
-echo -n "Okay, well, it's done. Finally, amirite? I trust it didn't really take years.  God, that would be awful.  I mean, you'd be all old, and stuff, and you probably wouldn't even have any use for this data anymore. I mean, unless it's one of those longitudinal things.  But I mean, come on. Wait, is it?  Is it one of those longitudinal things?  Well anyway, it's done, so what's next is up to you." | mail -s "The FreeSurfer segmentation process for ${ID} is COMPLETE!!!" ${EMAIL}
+echo -n "Recon-all of ${ID} has completed." | mail -s "The FreeSurfer segmentation process for ${ID} is COMPLETE!!!" ${EMAIL}
 fi
 exit
